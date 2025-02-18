979d56f446df293bc47a9106b1dce5c5d4d54f91
decatile <ohmycatile@gmail.com>
2025-02-18 19:33:48 +0300

    Base presenter logic

--- presenter/app.py
+++ presenter/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from .routes.auth import auth_router
+from .routes.referral_codes import referral_codes_router
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(referral_codes_router)
--- presenter/dependencies.py
+++ presenter/dependencies.py
@@ -1,8 +1,9 @@
-from typing import Annotated
+from typing import Annotated, AsyncGenerator
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.status import HTTP_403_FORBIDDEN
-from application.usecases.auth_user import AuthUserUseCase
+from application.usecases.auth_user import AuthUserUseCase, RefreshTokenSettings
 from application.usecases.create_referral_code import CreateReferralCodeUserCase
 from application.usecases.delete_referral_code import DeleteReferralCodeUserCase
 from application.usecases.get_referrals_by_referrer_id import (
@@ -10,27 +11,60 @@ from application.usecases.get_referrals_by_referrer_id import (
 )
 from application.usecases.register_user import RegisterUserUseCase
 from domain.entities.user import UserID
+from infrastructure.sql.repos.referral_code import SQLReferralCodeRepository
+from infrastructure.sql.repos.refresh_token import SQLRefreshTokenRepository
+from infrastructure.sql.repos.user import SQLUserRepository
+from infrastructure.sql.services.db import SQLDatabaseService
 from presenter.services.jwt import JwtService, JwtSettings
+from presenter.settings.db import DBSettings
 
 
-def get_register_use_case() -> RegisterUserUseCase:
-    raise NotImplementedError
+async def get_session() -> AsyncGenerator[AsyncSession]:
+    session = SQLDatabaseService.session(DBSettings())  # type: ignore
+    async with session.begin():
+        yield session
 
 
-def get_auth_use_case() -> AuthUserUseCase:
-    raise NotImplementedError
+GetSession = Annotated[AsyncSession, Depends(get_session)]
 
 
-def get_create_referral_code_use_case() -> CreateReferralCodeUserCase:
-    raise NotImplementedError
+def get_register_use_case(session: GetSession) -> RegisterUserUseCase:
+    return RegisterUserUseCase(
+        SQLUserRepository(session),
+        SQLRefreshTokenRepository(session),
+        SQLReferralCodeRepository(session),
+        RefreshTokenSettings(),  # type: ignore
+    )
 
 
-def get_delete_referral_code_use_case() -> DeleteReferralCodeUserCase:
-    raise NotImplementedError
+def get_auth_use_case(session: GetSession) -> AuthUserUseCase:
+    return AuthUserUseCase(
+        SQLUserRepository(session),
+        SQLRefreshTokenRepository(session),
+        RefreshTokenSettings(),  # type: ignore
+    )
 
 
-def get_referrals_by_referrer_id_use_ase() -> GetReferralsByReferrerIDUseCase:
-    raise NotImplementedError
+def get_create_referral_code_use_case(
+    session: GetSession,
+) -> CreateReferralCodeUserCase:
+    return CreateReferralCodeUserCase(
+        SQLUserRepository(session), SQLReferralCodeRepository(session)
+    )
+
+
+def get_delete_referral_code_use_case(
+    session: GetSession,
+) -> DeleteReferralCodeUserCase:
+    return DeleteReferralCodeUserCase(
+        SQLUserRepository(session), SQLReferralCodeRepository(session)
+    )
+
+
+def get_referrals_by_referrer_id_use_ase(
+    session: GetSession,
+) -> GetReferralsByReferrerIDUseCase:
+    return GetReferralsByReferrerIDUseCase(SQLUserRepository(session))
 
 
 def get_jwt_service() -> JwtService:
