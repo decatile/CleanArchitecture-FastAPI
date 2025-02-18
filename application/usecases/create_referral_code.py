b3a1dfc394f19b0caeecbefe34433cb120bd36a6
decatile <ohmycatile@gmail.com>
2025-02-18 20:25:04 +0300

    add lifetime validation

--- application/usecases/create_referral_code.py
+++ application/usecases/create_referral_code.py
@@ -1,4 +1,5 @@
 from application.exceptions.referral_code_already_exist import ReferralCodeAlreadyExist
+from application.exceptions.too_short_lifetime import TooShortLifetime
 from application.exceptions.user_not_found import UserNotFound
 from application.interfaces.referral_code_repository import ReferralCodeRepository
 from application.interfaces.user_repository import UserRepository
@@ -13,6 +14,8 @@ class CreateReferralCodeUserCase:
         self.referral_codes = referral_codes
 
     async def run(self, user_id: int, lifetime: int) -> ReferralCodeResponseDTO:
+        if lifetime < 60:
+            raise TooShortLifetime()
         user = await self.users.find_by_id(user_id)
         if user is None:
             raise UserNotFound()
